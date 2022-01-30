f3b72f7a1e26b7c4d86465316f2e0c5846c6f6f9
Jason <jay.garriques@gmail.com>
2022-01-30 00:23:04 -0500

    starting mongoose

---
+++ models/artist_model.js
@@ -0,0 +1,10 @@
+
+// ODM stands for Object Document Model. 
+// It translates the “documents” being stored in Mongo into fancier JS objects,
+// that have more helpful methods and properties. 
+// The ODM that we will use with MongoDB is Mongoose.
+// require mongoose
+//  use 
+// 
+
+// Schema: A Schema is a diagram or blueprint for what every object in the noSQL database will contain.
